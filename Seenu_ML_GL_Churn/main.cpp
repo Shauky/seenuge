7cc2f1cab8288d00041f5c05a4b8df6c604c8314
deviant <devi4nt@Shaukaths-MacBook-Pro.local>
2023-04-01 10:31:55 +0500

    first commit to SeenuGL game engine

---
+++ Seenu_ML_GL_Churn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Seenu_ML_GL_Churn
+//
+//  Created by deviant on 01/04/2023.
+//
+#include "../bin-int/test.hpp"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    GL::Print();
+    return 0;
+}
